e4f4065a6665973a21f4fbd28418503bea33c486
ramannikhil7 <134995338+ramannikhil7@users.noreply.github.com>
2023-06-19 14:21:42 +0530

    Update test msbuild path.ps1

--- test msbuild path.ps1
+++ test msbuild path.ps1
@@ -1,1 +1,2 @@
-&"${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe
+$ver = &"${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe
+Write-Host $ver
