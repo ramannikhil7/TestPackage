4afc66961cbf9e71781a97ae79b924ffedef3429
Raman Nikhil <111501090+ramannikhil@users.noreply.github.com>
2023-06-28 11:30:09 +0530

    added new tag

--- test_PS_script.ps1
+++ test_PS_script.ps1
@@ -1,1 +1,22 @@
 Write-Host "PS is running ...."
+
+# $version_tag = Invoke-Expression -Command 'git fetch --tags'
+$tags = Invoke-Expression -Command 'git tag --list'
+
+$latest_tag = ($tags | Sort-Object -Descending)[0]
+
+Write-Host "latest_tag : $latest_tag"
+
+
+$major, $minor, $patch = $latest_tag -split '\.'
+$major1 = $major.Replace("v", "")
+Write-Host "major $major1"
+Write-Host "minor $minor"
+Write-Host "patch $patch"
+
+
+Write-Host "$RELEASE_VERSION"
+
+
+
+
